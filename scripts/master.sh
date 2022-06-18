6c978237ff81400f35c0bdb89f52713c330ef94e
Atte Niemi <4998544+hur@users.noreply.github.com>
2022-06-18 19:39:57 +0100

    fix broken apt-get update

--- scripts/master.sh
+++ scripts/master.sh
@@ -25,8 +25,7 @@ sudo chsh -s /bin/bash $username
 # Update apt lists
 sudo apt-get update
 
-sudo apt-get -y install
-     git ccache automake flex lzop bison \
+sudo apt-get -y install git ccache automake flex lzop bison \
      gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 \
      g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev \
      libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush \
