96fe35c6180e6d6a91046ee8fe1dc9e22925d82d
Atte Niemi <4998544+hur@users.noreply.github.com>
2022-06-18 18:29:13 +0100

    install deps in master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -20,8 +20,16 @@ sudo chsh -s /bin/bash $username
 # Update apt lists
 sudo apt-get update
 
-# install repo
-sudo apt-get -y install repo
+sudo apt-get -y install
+     git ccache automake flex lzop bison \
+     gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 \
+     g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev \
+     libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush \
+     schedtool dpkg-dev liblz4-tool make optipng maven libssl-dev \
+     pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl \
+	 libxml-simple-perl bc libc6-dev-i386 lib32ncurses5-dev \
+     x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev xsltproc unzip \ 
+	 repo
 
 # get android sources 
 mkdir android-kernel && cd android-kernel
